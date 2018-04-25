54a513e3f49c474f390353240cbffeeb99e868b8
jma26 <j.masonoma17@gmail.com>
2018-04-25 01:11:49 -0700

    Add audio  sound in js, playSound error present

---
+++ index.js
@@ -0,0 +1,28 @@
+var audioA = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+      
+var audioB = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+
+var audioC = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+
+var audioD = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+
+
+$(document).ready(function() {
+    $("#a").click(function() {
+        audioA.play();
+    })
+    $("#b").click(function() {
+        audioB.play();
+    })
+    $("#c").click(function() {
+        
+        audioC.play();
+    })
+    $("#d").click(function() {
+       
+        audioD.play();
+    })
+})
+    
+    
+
