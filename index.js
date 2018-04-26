181f2267649101c3d9d6b5529ff5efb8aa940e29
jma26 <j.masonoma17@gmail.com>
2018-04-26 16:09:12 -0700

    Create standard simon object

--- index.js
+++ index.js
@@ -1,65 +1,38 @@
-var userLevel;
-var interval;
-var maxLevel = 20;
-simonSequence = [];
-userSequence = [];
+const RED = "RED";
+const BLUE = "BLUE";
+const YELLOW = "YELLOW";
+const GREEN = "GREEN";
 
-
-const audioA = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
-const audioB = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
-const audioC = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
-const audioD = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-
-gameStatus();
-
-
-
-function gameStatus() {
-    userLevel = 1;
-    interval = setInterval(function() {
-        setRandom();
-        userLevel--;
-    }, 1000);
-}
-
-// Set random number
-function setRandom() {
-    if (userLevel === 0) {
-        clearInterval(interval);
+var simon = {
+    sendColor: function(color) {
+        if (simon.sequence.length === 0) {
+            // start a new game
+        }
+    },
+    colors: [RED, BLUE, YELLOW, GREEN],
+    sequence: [],
+    nextSequence: function() {
+        var nextColor = simon.colors[Math.floor(Math.random() * simon.colors.length)];
+        simon.sequence.push(nextColor);
+        console.log("The sequence ", simon.sequence);
     }
-    let random = Math.floor(Math.random() * 4);
-    simonSequence.push(random);
-    console.log(simonSequence);
-    if (random === 0) {
-        setLevel("a", random);
-    } else if (random === 1) {
-        setLevel("b", random);
-    } else if (random === 2) {
-        setLevel("c", random);
-    } else if (random === 3) {
-        setLevel("d", random);
-    }
-}
+};
 
-function setLevel(id, random) {
-    $("#" + id).css("opacity", "1");
-    playSound(random);
-    setTimeout(function() {
-        $("#" + id).css("opacity", "0.6");
-    }, 1000);
-}
+$(document).ready(function() {
+    $("#green").click(function() {
+        simon.sendColor(GREEN);
+    });
+    $("#red").click(function() {
+        simon.sendColor(RED);
+    });
+    $("#blue").click(function() {
+        simon.sendColor(BLUE);
+    });
+    $("#yellow").click(function() {
+        simon.sendColor(YELLOW);
+    });
+})
 
-function playSound(num) {
-    if (num === 0) {
-        audioA.play();
-    } else if (num === 1) {
-        audioB.play();
-    } else if (num === 2) {
-        audioC.play();
-    } else if (num === 3) {
-        audioD.play();
-    } 
-}
 
 
 
