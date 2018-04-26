1f591c23c11ae0615f80b81b96e5ff4e50049b86
jma26 <j.masonoma17@gmail.com>
2018-04-26 13:16:09 -0700

    Add function playSound, setLevel, setRandom, gameStatus

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
+var userLevel;
+var interval;
+var maxLevel = 20;
+simonSequence = [];
+userSequence = [];
 
 
-$(document).ready(function() {
-    var audioA = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
-          
-    var audioB = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
-    
-    var audioC = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
-    
-    var audioD = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+const audioA = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+const audioB = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+const audioC = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+const audioD = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
-    $("#a").click(function() {
+gameStatus();
+
+
+
+function gameStatus() {
+    userLevel = 1;
+    interval = setInterval(function() {
+        setRandom();
+        userLevel--;
+    }, 1000);
+}
+
+// Set random number
+function setRandom() {
+    if (userLevel === 0) {
+        clearInterval(interval);
+    }
+    let random = Math.floor(Math.random() * 4);
+    simonSequence.push(random);
+    console.log(simonSequence);
+    if (random === 0) {
+        setLevel("a", random);
+    } else if (random === 1) {
+        setLevel("b", random);
+    } else if (random === 2) {
+        setLevel("c", random);
+    } else if (random === 3) {
+        setLevel("d", random);
+    }
+}
+
+function setLevel(id, random) {
+    $("#" + id).css("opacity", "1");
+    playSound(random);
+    setTimeout(function() {
+        $("#" + id).css("opacity", "0.6");
+    }, 1000);
+}
+
+function playSound(num) {
+    if (num === 0) {
         audioA.play();
-    })
-    $("#b").click(function() {
+    } else if (num === 1) {
         audioB.play();
-    })
-    $("#c").click(function() {
-        
+    } else if (num === 2) {
         audioC.play();
-    })
-    $("#d").click(function() {
-       
+    } else if (num === 3) {
         audioD.play();
-    })
-})
+    } 
+}
+
+
+
     
     
 
