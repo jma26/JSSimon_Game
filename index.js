cb21a3676f3613ea2d8db6ae24d34781df53f092
jma26 <j.masonoma17@gmail.com>
2018-04-25 21:21:27 -0700

    Update button with opacity for contrast when active

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var audioA = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
-      
-var audioB = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
-
-var audioC = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
-
-var audioD = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
 
 $(document).ready(function() {
+    var audioA = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+          
+    var audioB = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+    
+    var audioC = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+    
+    var audioD = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+
     $("#a").click(function() {
         audioA.play();
     })
